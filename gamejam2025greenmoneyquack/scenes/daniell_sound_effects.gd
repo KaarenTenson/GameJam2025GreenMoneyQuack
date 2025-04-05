381108aa5f114583fcbcfe1c41e26ecc71aed74e
KaarenTenson <tensonkaaren@gmail.com>
2025-04-05 23:00:44 +0300

    audio

---
+++ gamejam2025greenmoneyquack/scenes/daniell_sound_effects.gd
@@ -0,0 +1,12 @@
+extends AudioStreamPlayer
+enum CLIPS {ÄRA_LIIGUTA, JAH_ARUSAADAV, JÄRGMISE_KORRANI, KORVPALLI_TIIMI, TEIN_ON_AINA_ROHKEM}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Global.debtInc.connect(func(): play(CLIPS.ÄRA_LIIGUTA))
+	Global.hpDec.connect(func(): play(CLIPS.JAH_ARUSAADAV))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
