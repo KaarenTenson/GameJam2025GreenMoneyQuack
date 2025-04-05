10fa83c1d6946ac2f6000d86713ebf205123febf
Kauri <kauriremm@gmail.com>
2025-04-05 20:54:47 +0300

    wall generator

--- gamejam2025greenmoneyquack/scripts/pizza/pizza_hit_box.gd
+++ gamejam2025greenmoneyquack/scripts/pizza/pizza_hit_box.gd
@@ -5,7 +5,7 @@ func _on_area_entered(area: Area2D) -> void:
 	#print("collided")
 	if area is JamHitBox:
 		area.hit(get_parent())
-	elif area is wall:
+	elif area is wall_area:
 		print("wall")
 		Global.add_debt()
 		get_parent().destroy()
