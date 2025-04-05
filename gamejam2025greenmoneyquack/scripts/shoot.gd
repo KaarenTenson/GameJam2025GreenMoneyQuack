49893e8a0a276d4608bcc388d433509e920237d4
Kauri <kauriremm@gmail.com>
2025-04-05 11:58:19 +0300

    player shoot partial

---
+++ gamejam2025greenmoneyquack/scripts/shoot.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+func _input(event):
+	if event.is_action_pressed("shoot_meat"):
+		print("shoot_meat")
+		var mouse_pos:Vector2 = get_viewport().get_mouse_position()
+		
+		
+	if event.is_action_pressed("shoot_vegan"):
+		print("shoot_vegan")
+
+func shoot(mouse:Vector2):
+	pass
