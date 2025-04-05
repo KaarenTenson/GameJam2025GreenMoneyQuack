637bf443dbce26efb208cf32a19ba00ebcf2d160
Rasmus <145966408+Virteso@users.noreply.github.com>
2025-04-05 22:32:56 +0300

    Pizza rotate

--- gamejam2025greenmoneyquack/scripts/pizza/pizza.gd
+++ gamejam2025greenmoneyquack/scripts/pizza/pizza.gd
@@ -10,6 +10,7 @@ var direction: Vector2
 var speed: float = 2000.0  # Speed in pixels per second
 
 @export var pizza = global.pizza.MEAT
+@export var angularSpeed: float = -30
 
 
 func set_new_position(fly: Vector2) -> void:
@@ -19,6 +20,7 @@ func _process(delta: float) -> void:
 	if direction != null:
 		position += direction.normalized() * speed * delta
 		#print(position)
+	rotate(angularSpeed * delta)
 
 func _ready() -> void:
 	set_new_position(Vector2(1200,0))
