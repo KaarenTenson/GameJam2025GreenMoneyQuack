63f32513797755ca999ce478b9898f24ad866ae4
Kauri <kauriremm@gmail.com>
2025-04-06 13:41:05 +0300

    ootaja saab oodata hiire clicki

--- gamejam2025greenmoneyquack/scripts/aeglustaja.gd
+++ gamejam2025greenmoneyquack/scripts/aeglustaja.gd
@@ -3,6 +3,33 @@ class_name Aeglustaja
 
 @export var milleks_aeglustada = 0
 
+@export var oota_klick = false
+
+var acc = 20
+
+var ootab_klick = false
+
+var player:Player
+
 func _on_area_entered(area: Area2D) -> void:
+	
+		
+		
 	if(area.get_parent() is Player):
-		area.get_parent().move_speed = milleks_aeglustada
+		player = area.get_parent()
+		if oota_klick:
+			acc = player.accseleration
+			player.accseleration = 0
+			player.move_speed = milleks_aeglustada
+			ootab_klick = true
+		else:
+			player.move_speed = milleks_aeglustada
+
+func _input(event:InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if ootab_klick:
+			set_acc(acc)
+
+func set_acc(temp):
+	player.accseleration = acc
+	
