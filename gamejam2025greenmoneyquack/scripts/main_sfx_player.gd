27ebd2cd9f00d19e7511e20c3b90cbed59292fb3
Rasmus <145966408+Virteso@users.noreply.github.com>
2025-04-06 11:33:31 +0300

    Sound components

--- gamejam2025greenmoneyquack/scripts/main_sfx_player.gd
+++ gamejam2025greenmoneyquack/scripts/main_sfx_player.gd
@@ -8,8 +8,4 @@ func play_track(id : int) -> void:
 	play()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
-	Global.hitCorrect.connect(func (pos): play_track(CLIPS.EAT))
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	Global.hitCorrect.connect(func (_pos): play_track(CLIPS.EAT))
