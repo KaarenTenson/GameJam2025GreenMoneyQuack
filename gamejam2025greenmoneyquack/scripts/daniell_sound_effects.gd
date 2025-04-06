a825bad518dc5c2bcaea7308d8a6ddd30f97128a
KaarenTenson <tensonkaaren@gmail.com>
2025-04-06 10:29:32 +0300

    audio fix

--- gamejam2025greenmoneyquack/scripts/daniell_sound_effects.gd
+++ gamejam2025greenmoneyquack/scripts/daniell_sound_effects.gd
@@ -1,7 +1,13 @@
 extends AudioStreamPlayer
 enum CLIPS {ÄRA_LIIGUTA, JAH_ARUSAADAV, JÄRGMISE_KORRANI, KORVPALLI_TIIMI, TEIN_ON_AINA_ROHKEM}
 
+@export var streams : Array[AudioStream]
+
+func play_track(id : int) -> void:
+	stop()
+	stream = streams[id]
+	play()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Global.debtInc.connect(func(): play(CLIPS.ÄRA_LIIGUTA))
-	Global.hpDec.connect(func(): play(CLIPS.JAH_ARUSAADAV))
+	Global.debtInc.connect(func(): play_track(CLIPS.ÄRA_LIIGUTA))
+	Global.hpDec.connect(func(): play_track(CLIPS.JAH_ARUSAADAV))
