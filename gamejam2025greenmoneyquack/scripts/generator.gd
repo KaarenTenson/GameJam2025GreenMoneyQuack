fdc432ec2a2c1a6f3f24861680915f2b1a2af513
KaarenTenson <tensonkaaren@gmail.com>
2025-04-06 14:19:27 +0300

    parandused

--- gamejam2025greenmoneyquack/scripts/generator.gd
+++ gamejam2025greenmoneyquack/scripts/generator.gd
@@ -1,7 +1,6 @@
 extends Node2D
 class_name generator
 
-var jammer_scene = preload("res://scenes/jammer.tscn")
 
 @export var spawn_chance = 0.7
 @export var vegan_chance = 0.2
@@ -14,16 +13,17 @@ var weights = PackedFloat32Array([1])
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#decide()
-	pass # Replace with function body.
+	decide()
+	#pass # Replace with function body.
 
 
 func decide():
+	await ($"../../player" as Player).draw
 	var spawned = randf() <= spawn_chance
 	if !spawned:
 		return
 	
-	var jammer:Jammer = jammer_scene.instantiate()
+	var jammer:Jammer = (get_parent() as Generators).jammer_scene.instantiate()
 	jammer.global_position = global_position
 	
 	var vegan = randf() < vegan_chance
@@ -44,7 +44,6 @@ func decide():
 	
 	$"../../Jammijad".add_child(jammer)
 	print("Generated " + str(jammer.pizza))
-
-func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
-	decide()
 	queue_free()
+	
+	
