a41748536e4ba42b05e341748a6f96cf1c57a70d
Kauri <kauriremm@gmail.com>
2025-04-06 12:37:07 +0300

    aeglustaja export milleks aeglustada

--- gamejam2025greenmoneyquack/scripts/aeglustaja.gd
+++ gamejam2025greenmoneyquack/scripts/aeglustaja.gd
@@ -1,7 +1,8 @@
 extends Area2D
 class_name Aeglustaja
 
+@export var milleks_aeglustada = 0
 
 func _on_area_entered(area: Area2D) -> void:
 	if(area.get_parent() is Player):
-		area.get_parent().move_speed = 0
+		area.get_parent().move_speed = milleks_aeglustada
