b158f295bc1ccc8bf24b5226491001048bb8056c
Rasmus <145966408+Virteso@users.noreply.github.com>
2025-04-06 10:19:08 +0300

    Tutorial 2

--- gamejam2025greenmoneyquack/scripts/finish.gd
+++ gamejam2025greenmoneyquack/scripts/finish.gd
@@ -1,4 +1,7 @@
 extends Area2D
+
+@export var path: String = "res://scenes/win_screen.tscn"
+
 func _on_area_entered(area: Area2D) -> void:
 	if(area.get_parent() is Player):
-		get_tree().change_scene_to_file("res://scenes/win_screen.tscn")
+		get_tree().change_scene_to_file(path)
