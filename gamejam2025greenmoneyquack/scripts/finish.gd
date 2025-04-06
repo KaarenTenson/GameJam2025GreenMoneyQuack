92bd06d8bbca7805081e04ddb5deeadce425a281
Kauri <kauriremm@gmail.com>
2025-04-06 15:36:18 +0300

    push

--- gamejam2025greenmoneyquack/scripts/finish.gd
+++ gamejam2025greenmoneyquack/scripts/finish.gd
@@ -8,6 +8,7 @@ func _on_area_entered(area: Area2D) -> void:
 		Global.reset()
 		(area.get_parent() as Player).move_speed=0
 		(area.get_parent() as Player).accseleration=0
-		ani_player.play("end_animation")
-		await ani_player.animation_finished
+		if ani_player:
+			ani_player.play("end_animation")
+			await ani_player.animation_finished
 		get_tree().change_scene_to_file(path)
